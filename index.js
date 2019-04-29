fdd15c24d7be09914778ba5840d98bf03b1f1998
Volker Eichhorn <mail@volker.io>
2019-04-29 17:17:17 +0200

    Add support for forcePathStyle

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const pkg = require('./package.json');
 
 const register = (server, option) => {
     const config = joi.attempt(option, joi.object().required().keys({
-        bucket    : joi.string().required().hostname().min(1),
-        publicKey : joi.string().required().token().min(20),
-        secretKey : joi.string().required().base64().min(40),
-        endpoint  : joi.string().optional()
+        bucket         : joi.string().required().hostname().min(1),
+        publicKey      : joi.string().required().token().min(20),
+        secretKey      : joi.string().required().base64().min(40),
+        endpoint       : joi.string().optional(),
+        forcePathStyle : joi.boolean().default(false).optional()
     }));
 
     server.decorate('server', 's3', new Scube(config));
